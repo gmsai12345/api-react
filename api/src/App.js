f7d9eb02107179a8074b5d8eda40e773c4867f36
Sai Dhakshan Y <112693557+gmsai12345@users.noreply.github.com>
2022-12-22 13:42:08 +0530

    Update App.js

--- api/src/App.js
+++ api/src/App.js
@@ -9,7 +9,7 @@ function App() {
   const fetchData = async () => {
     const response = await axios.get(
       "https://www.anapioficeandfire.com/api/books?pageSize=30"
-    );
+    ); //  the url having dR
 
     setBooks(response.data);
   };
@@ -21,25 +21,26 @@ function App() {
 
       {/* Fetch data from API */}
       <div>
-        <button className="fetch-button" onClick={fetchData}>
+        <button className="fetch-button" onClick={fetchData}> // fetch data
           Fetch Data
         </button>
         <br />
       </div>
 
       {/* Display data from API */}
-      <div className="books">
+      <div className="books"> // how to display the data 
         {books &&
-          books.map((book, index) => {
+          books.map((book, index) => { // mapping so that it can be converted from array to each element which you want to be displayed in the output
             const cleanedDate = new Date(book.released).toDateString();
-            const authors = book.authors.join(", ");
+            const authors = book.authors.join(", "); // joining authors
 
             return (
-              <div className="book" key={index}>
-                <h3>Book {index + 1}</h3>
-                <h2>{book.name}</h2>
+              <div className="book" key={index}> // displaying index from 1 to 8
+                <h3>Book {index + 1}</h3> 
+                <h2>{book.name}</h2> 
+// from name element in array
 
-                <div className="details">
+                <div className="details"> // details of the book such as author,pages,country,day it was published in small chart like format in webpage
                   <p>{authors}</p>
                   <p>{book.numberOfPages} pages</p>
                   <p>🏘{book.country}</p>
